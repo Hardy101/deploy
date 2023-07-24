ef463af2c09c2f43172732a2cf4a00610f0d3bf6
Eghosa Ordia <eghordia130@gmail.com>
2023-07-24 16:25:58 +0100

    REFACTOR

--- app/main.py
+++ app/main.py
@@ -81,5 +81,5 @@ def notfound(page):
     return render_template('pages/notFound.html')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    # if __name__ == "__main__":
+    #     app.run(debug=True)
